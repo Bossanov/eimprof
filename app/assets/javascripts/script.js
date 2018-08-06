daee3dd5b2c2b6f9181b0fe0330f0596c5dd11ee
Antoine STAUMONT <staumont.antoine@me.com>
2018-08-07 00:22:34 +0200

    theme ok heroku ok

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -57,21 +57,21 @@ include('/assets/jquery.mobilemenu.js');
 ========================================================*/
 $(function(){
 // IPad/IPhone
-  var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
-  ua = navigator.userAgent,
+	var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+	ua = navigator.userAgent,
 
-  gestureStart = function () {viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";},
+	gestureStart = function () {viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";},
 
-  scaleFix = function () {
-    if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
-      viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-      document.addEventListener("gesturestart", gestureStart, false);
-    }
-  };
+	scaleFix = function () {
+		if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+			viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+			document.addEventListener("gesturestart", gestureStart, false);
+		}
+	};
 
-  scaleFix();
-  // Menu Android
-  if(window.orientation!=undefined){
+	scaleFix();
+	// Menu Android
+	if(window.orientation!=undefined){
   var regM = /ipod|ipad|iphone/gi,
    result = ua.match(regM)
   if(!result) {
